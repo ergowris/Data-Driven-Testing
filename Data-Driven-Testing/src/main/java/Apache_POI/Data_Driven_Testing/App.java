9fa6054fb46a31fb9a284bab022b801db32fc460
Gowri <Gowri@DESKTOP-AF33508.Dlink>
2023-09-20 17:12:21 +0530

    created data driven testing project using apache poi

---
+++ Data-Driven-Testing/src/main/java/Apache_POI/Data_Driven_Testing/App.java
@@ -0,0 +1,13 @@
+package Apache_POI.Data_Driven_Testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
